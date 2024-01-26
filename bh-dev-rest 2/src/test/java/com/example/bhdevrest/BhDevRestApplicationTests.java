8f95f414c8fe1f318934b7f1e117f4c1e7a94180
rahul <azuredevopst@gmail.com>
2024-01-27 00:12:01 +0530

    first commit

---
+++ bh-dev-rest 2/src/test/java/com/example/bhdevrest/BhDevRestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.bhdevrest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BhDevRestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
