a392bb19f834dda7040da9be6e646fe9d56fe71d
rahul <azuredevopst@gmail.com>
2024-02-08 00:28:56 +0530

    code up

---
+++ futura/src/main/java/com/example/futura/FuturaApplication.java
@@ -0,0 +1,13 @@
+package com.example.futura;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FuturaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FuturaApplication.class, args);
+	}
+
+}
---
+++ futura/src/test/java/com/example/futura/FuturaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.futura;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FuturaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
